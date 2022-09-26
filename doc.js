acda33417f29f7693ca4e8a7793222f61327f61a
Are-Jae <evansraven7@gmail.com>
2022-09-25 20:45:58 -0400

    attempt to push js

---
+++ doc.js
@@ -0,0 +1,7 @@
+var startButton= document.querySelector("start-button");
+var quizQuestions= document.querySelector("questions");
+var quizResults=  document.querySelector("results");
+var quizScores= document.querySelector("scores");
+var quizTimer= document.querySelector("timer-text");
+var countDown= 0;
+
