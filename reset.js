b471b1ac870c2f5dc9a4bbed3d77cd25b7bd9dce
Are-Jae <evansraven7@gmail.com>
2023-05-05 08:39:52 -0400

    updated code, need to fix questions funcx

--- reset.js
+++ reset.js
@@ -1,44 +1,27 @@
-const quiz = document.querySelector("#Startquiz");//function for quiz 
-const startBtn = document.querySelector("#quizBtn"); //starts quiz after button is clicked, will be inside of Startquiz  
+const startBtn = document.querySelector("#quizBtn");
 
-
-
-const quiztimer = document.querySelector("#time");//to be added after Startquiz funcx 
-const question = document.querySelector("#Startquest");//will start asking questions from array after "Start Quiz" is selected 
-const choices = document.querySelector("#Choicestart");//will display choices for answers from array then save the user choice to localStorage 
-
-let message = document.querySelector("#Answermessage")//shows wrong/correct on DOM 
-const storedScores = localStorage.getItem("storedScores"); //stores highscores to local storage 
+const quiztimer = document.querySelector("#time");
+const question = document.querySelector("#Startquest");
+const choices = document.querySelector("#Choicestart");
+let message = document.querySelector("#Answermessage")
+const storedScores = localStorage.getItem("storedScores");
 const localScores = storedScores ? JSON.parse(storedScores) : [];
 
+const end = document.querySelector("#endQuiz");
+let submitBtn = document.createElement("button");
+let btnMessage = "Submit";
+submitBtn.textContent = btnMessage;
 
-const end = document.querySelector("#endQuiz"); //will be new funcx to end quiz & submit scores
-let submitBtn = document.createElement("button"); //createElement within endQuiz funcx 
-let btnMessage = "Submit"; //will append using submitBtn.textContent= btnMessage (or innerHTML?)
+let highscoresMessage = document.querySelector("#noScores")
+let previousScoresmessage = "There are no previously saved scores"
 
-
-
-let highscoresMessage = document.querySelector("#noScores")//will be in highscores html, will display if no prev HS saved 
-let previousScoresmessage = "There are no previously saved scores" //self explanatory 
-
-const index = 0; //index must always be 0 
-let score = ''; //set as empty string for data to pass through 
+const index = 0;
+let score = 0;
 let correctAnswer = "Correct";
 let wrongAnswer = "Wrong";
 
+let savedScores = "";
 
-
-let savedScores = "";  //set as empty string for data to pass through 
-
-
-//I need a funcx that stops the questions and saves user score to local storage after they select "finish quiz" 
-//Once that funcx has run I need a funcx that prompts user to write their initials in a textbox & I need that funcx to save the initials to local storage after they finish the quiz 
-//I need Highscores to open into its own HTML, it also needs to have access to local storage for previous user's scores/initials 
-
-
-
-
-//arr for questions and answers 
 let set = [
     {
         question: "Which of these is not a logical operator?",
@@ -47,7 +30,7 @@ let set = [
     },
     {
         question: "What is not an event type?",
-        choices: ["Click", "Mousemove", "Cocktail Party",],
+        choices: ["Click", "Mousemove", "Cocktail Party"],
         correct: "Cocktail Party",
     },
     {
@@ -72,110 +55,74 @@ let set = [
     }
 ]
 
+let quizTimer = null;
+let timeLeft = 60;
 
-
-
-
-
-
-let quizTimer= ""  //starts timer
-
-let timeLeft=  "" //var keeps track of seconds left in quiz 
-
-function Startquiz(){
-
-quizBtn.addEventListener("click", function(){
-
-
-    function startTimer(){
-
-        quizTimer = setInterval( function () {
-
-        
-        
+function startTimer() {
+    quizTimer = setInterval(function() {
         if (timeLeft > 0) {
-        
-        
-        timeLeft--
-        
+            timeLeft--;
+            quiztimer.textContent = timeLeft;
+        } else {
+            clearInterval(quizTimer);
+            quizTimer = null;
+            endQuiz();
         }
-        
-        
-    }, 1000); 
-        
-        }
-        
-        clearInterval(quizTimer); 
-    }
-
-)};
-
-
-
-
-    question.innerHTML = ""; //grabbing questions from arr 
-    choices.innerHTML = ""; //grabbing choices from arr 
-    // looping through the set array
-	
-    var currentQuestion = set[index].question 
-    question.innerHTML = currentQuestion
-    var currentAnswer = set[index].choices
-	
-    currentAnswer.forEach(function (i) {
-        var button = document.createElement("button")
-        button.innerHTML = i;
-        choices.append(button)
-        button.addEventListener("click", function (event) {
-            event.preventDefault()
-            let clicked = event.target;
-            if (clicked.innerHTML) {
-                if (clicked.innerHTML == set[index].correct) {
-                    score = score + 1
-					
-                    console.log("Correct");
-                    message.textContent= correctAnswer;     //something in here should change to show correct/wrong on the DOM rinse & repeat; done  
-                } else {
-                    score = score - 1
-                    console.log("Wrong");
-                    message.textContent= wrongAnswer;
-                    if (clicked.innerHTML == set[index].correct){
-                        score <= 0
-                 
-				        
-                        console.log("Wrong");
-                        message.textContent=wrongAnswer;
-                    }
-                }
-                index++
-                if(index < set.length) {
-                     //index = [0]
-                    start(index)
-                } else { 
-				
-				event.stopPropagation(); 
+    }, 1000);
+}
+function startQuiz() {
+    startTimer();
+    question.innerHTML = "";
+    choices.innerHTML = "";
+    var currentQuestion = set[index].question;
+    question.innerHTML = currentQuestion;
+    var currentAnswer = set[index].choices;
+    currentAnswer.forEach(function start(i) {
+      var button = document.createElement("button");
+      button.innerHTML = i;
+      choices.append(button);
+      button.addEventListener("click", function(event) {
+        event.preventDefault();
+        let clicked = event.target;
+        if (clicked.innerHTML) {
+            if (clicked.innerHTML == set[index].correct) {
+                score = score + 1
+                
+                console.log("Correct");
+                message.textContent= correctAnswer;     //something in here should change to show correct/wrong on the DOM rinse & repeat; done  
+            } else {
+                score = score - 1
+                console.log("Wrong");
+                message.textContent= wrongAnswer;
+                if (clicked.innerHTML == set[index].correct){
+                    score <= 0
+             
                     
-                localStorage.setItem("scores");
+                    console.log("Wrong");
+                    message.textContent=wrongAnswer;
                 }
             }
-        })
-    })
-   
+            index++;
+            if(index < set.length) {
+                 //index = [0]
+                start(index)
+            } else { 
+            
+                clearInterval(quizTimer);
+                quizTimer = null;
+                endQuiz();
+                
+            
+            }
+            function endQuiz() {
+                localStorage.setItem("storedScores", JSON.stringify(storedScores)); 
+              }   }
+    
+    });
+});
 
+} 
+  startBtn.addEventListener("click", startQuiz);
 
-
-
-
-
-
-localScores = {
-
-initials: '', 
-
-scores: '',
-
-
-}
-
-//object to store initials and scores, empty strings to pass data through 
-
-// storedScore.push will add info from localstorage into Highscores list 
+  //after answer made quiz does not move to next queston, why?
+  //is submit button working?
